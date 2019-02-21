ac97dc59c8c79dd4bc0a04f524a816d20a8f1db0
beniamin-benek <46487463+beniamin-benek@users.noreply.github.com>
2019-02-21 15:04:57 +0100

    Update ArrayUtils.java

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,13 +1,13 @@
 import java.util.List;
 
-public class ArrayUtils {
+class ArrayUtils {
 
-	public static void printReversed(List<Integer> array) {
+	static void printReversed(List<Integer> array) {
 		for (int i = array.size() - 1; i >= 0; i--)
 			System.out.print(array.get(i) + ", ");
 	}
 	
-	public static void printSum(List<Integer> array) {
+	static void printSum(List<Integer> array) {
 		if (array == null || array.size() == 0) throw new NullPointerException();
 		
 		int sum = 0;
@@ -15,16 +15,17 @@ public class ArrayUtils {
 			sum = sum + array.get(i);
 
 		System.out.print("\nSuma : ");
-		for(Integer num: array) {
-			if (!((num == array.get(array.size() - 1))))
-				System.out.print(num + " + ");
+		for (int i = 0; i < array.size(); i++) {
+			if (i != array.size() - 1)
+				System.out.print(array.get(i) + " + ");
 			else
-				System.out.print(num + " = " + sum);
+				System.out.print(array.get(i) + " = " + sum);
 		}
 		System.out.println();
+
 	}
 	
-	public static void printTheSmallest(List<Integer> array) {
+	static void printTheSmallest(List<Integer> array) {
 		if (array == null || array.size() == 0) throw new NullPointerException();
 		
 		int min = array.get(0);
@@ -35,7 +36,7 @@ public class ArrayUtils {
 		System.out.println("Najmniejszy element: " + min);
 	}
 	
-	public static void printTheBiggest(List<Integer> array) {
+	static void printTheBiggest(List<Integer> array) {
 		
 		if (array == null || array.size() == 0) throw new NullPointerException();
 		
